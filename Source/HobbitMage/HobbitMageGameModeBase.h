e2eea72f9f586ae322bf148f61c8a6f819b1483d
Andrius <andr.paulauskas@gmail.com>
2019-01-26 09:32:13 +0200

    initial project commit

---
+++ Source/HobbitMage/HobbitMageGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HobbitMageGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOBBITMAGE_API AHobbitMageGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
